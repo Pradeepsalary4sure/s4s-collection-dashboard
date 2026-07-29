51eb4c382cc4d1e679357ab824bce41b06e20376
Pradeep <pradeep@salary4sure.com>
2026-07-29 21:37:19 +0530

    Resolve merge conflicts

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,3 +1,28 @@
+// import { defineConfig, loadEnv } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, process.cwd(), "");
+
+//   return {
+//     plugins: [react(), tailwindcss()],
+//     server: {
+//       proxy: {
+//         "/api": {
+// <<<<<<< HEAD
+//           // target: env.VITE_API_PROXY_TARGET || "http://localhost:5000",
+// =======
+//           target: env.VITE_API_PROXY_TARGET || "https://s4s-collection-dashboard.onrender.com/",
+// >>>>>>> 20ab99bc6a7a9ba4182eb2aa8c0ad955d294fcfc
+//           changeOrigin: true,
+//         },
+//       },  
+//     },
+//   };
+// });
+
+
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -10,14 +35,12 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         "/api": {
-<<<<<<< HEAD
-          target: env.VITE_API_PROXY_TARGET || "http://localhost:5000",
-=======
-          target: env.VITE_API_PROXY_TARGET || "https://s4s-collection-dashboard.onrender.com/",
->>>>>>> 20ab99bc6a7a9ba4182eb2aa8c0ad955d294fcfc
+          target:
+            env.VITE_API_PROXY_TARGET ||
+            "https://s4s-collection-dashboard.onrender.com",
           changeOrigin: true,
         },
-      },  
+      },
     },
   };
 });
